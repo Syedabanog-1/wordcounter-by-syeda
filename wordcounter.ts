50ed857cea0d1998c04cc7386391edaf3feb9cba
Syedabanog-1 <160377198+Syedabanog-1@users.noreply.github.com>
2024-04-21 15:46:47 +0500

    Add files via upload

---
+++ wordcounter.ts
@@ -0,0 +1,10 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+
+let user_answer = await inquirer.prompt({
+    type: "input",
+    name: "words",
+    message: "Please enter your sentence:"
+})
+let word_count =user_answer.words.trim().split(" ").length //teim method will remove white spaces and split will  bring words in array form
+console.log(` Your sentence contains ${word_count} words.`); 
